be653cde6770d1113a93e23bebf33d7dc93639fd
Keenan Yafiq <accbruh0@gmail.com>
2024-08-09 15:48:13 +0700

    IP count correction & fix slight issue on buffer-version script

--- generate4BillionIPsBuffer.js
+++ generate4BillionIPsBuffer.js
@@ -2,29 +2,27 @@
 // 128 GBs of RAM are probably required.
 
 const fs = require('fs');
-const generatedIPs = new Set();
-const IP_COUNT = Math.pow(2, 32);
+const IP_COUNT = Math.pow(2, 32) + 1;
 const LOG_INTERVAL = Math.floor(IP_COUNT / 100); // Log progress every 1% completion
 
-function generateIP() {
-    let generatedIP;
-    do {
-        generatedIP = `${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}`;
-    } while (generatedIPs.has(generatedIP));
-
-    generatedIPs.add(generatedIP);
-    return generatedIP;
+function intToIP(int) {
+    return [
+        (int >>> 24) & 0xFF,
+        (int >>> 16) & 0xFF,
+        (int >>> 8) & 0xFF,
+        int & 0xFF
+    ].join('.');
 }
 
 // Use a buffer to store IPs
 let ipBuffer = [];
 for (let i = 1; i <= IP_COUNT + 1; i++) {
-    ipBuffer.push(generateIP());
+    ipBuffer.push(intToIP(i));
 
     // Log progress at intervals
-    if (i % LOG_INTERVAL === 0) console.log(`[Progress] Generated ${i} / ${IP_COUNT} IPs (${(i / IP_COUNT) * 100}%)`);
+    if (i % LOG_INTERVAL == 0) console.log(`[Progress] Generated ${i} / ${IP_COUNT} IPs (${(i / IP_COUNT) * 100}%)`);
 }
 
 // Write the generated IPs to the file
-fs.writeFileSync("./4BillionIPs.txt", ipBuffer.join("\n"));
+fs.writeFileSync("./4BillionIPss.txt", ipBuffer.join("\n"));
 console.log("IP generation complete and saved to file.");
